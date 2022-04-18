b1889ec6c448844cd04fd77ea68754ef61f9fbc0
Ratofante <gwilkens.r@gmail.com>
2022-04-18 16:03:56 -0300

    header & banner andando

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const menuBtn = document.getElementById('menuBtn');
+const navBar = document.getElementById('navBar');
+menuBtn.addEventListener('click', function () {
+   if (navBar.classList.contains('closed-menu')) {
+      navBar.classList.remove('closed-menu');
+      menuBtn.setAttribute('aria-expanded', 'true');
+      navBar.classList.add('open-menu');
+
+   } else {
+      navBar.classList.remove('open-menu');
+      menuBtn.setAttribute('aria-expanded', 'false');
+      navBar.classList.add('closed-menu');
+   }
+})
