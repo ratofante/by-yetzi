5d3344805a19aa026f46602e0752cac2dcd8721f
yanina <yania.dabrowski@hotmail.com.ar>
2022-05-12 00:17:53 -0300

    arreglo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const menuBtn = document.getElementById('menuBtn');
 const navBar = document.getElementById('navBar');
-menuBtn.addEventListener('click', function () {
+menuBtn.addEventListener('click', function (e) {  
+   e.preventDefault()
    if (navBar.classList.contains('closed-menu')) {
       navBar.classList.remove('closed-menu');
       menuBtn.setAttribute('aria-expanded', 'true');
